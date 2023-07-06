dd5559e14b9641473cb2bd472ebc1717090c01c4
Alex Kireeff <alexkireeff@gmail.com>
2023-07-05 23:51:44 -0400

    now columns (x,y,z) match with dimensions
    and new column a_col is what we are measuring
    NOTE can graph at 3 dimensions by setting z_col = a_col

--- users/alex/analysis/scatter_plot.py
+++ users/alex/analysis/scatter_plot.py
@@ -8,37 +8,37 @@ import pandas as pd
 
 
 
-def save_rotated_scatter_plot(df_list, shortened_parquet_filenames, plot_dir, w_col, x_col, y_col, z_col, step_size, dimension_limits):
+def save_rotated_scatter_plot(df_list, shortened_parquet_filenames, plot_dir, x_col, y_col, z_col, a_col, step_size, dimension_limits):
     # Combine the parquet files
     df_combined = pd.concat(df_list)
 
     # Extract the column values
-    w = df_combined[w_col].values
     x = df_combined[x_col].values
     y = df_combined[y_col].values
     z = df_combined[z_col].values
+    a = df_combined[a_col].values
 
     # Create the 3D scatter plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    scatter = ax.scatter(w, x, y, c=z, cmap="hot")
-    ax.set_xlabel(w_col)
-    ax.set_ylabel(x_col)
-    ax.set_zlabel(y_col)
+    scatter = ax.scatter(x, y, z, c=a, cmap="hot")
+    ax.set_xlabel(x_col)
+    ax.set_ylabel(y_col)
+    ax.set_zlabel(z_col)
 
     # Set the minimum and maximum values for each dimension
-    if w_col in dimension_limits:
-        ax.set_xlim(dimension_limits[w_col]["min"], dimension_limits[w_col]["max"])
     if x_col in dimension_limits:
-        ax.set_ylim(dimension_limits[x_col]["min"], dimension_limits[x_col]["max"])
+        ax.set_xlim(dimension_limits[x_col]["min"], dimension_limits[x_col]["max"])
     if y_col in dimension_limits:
-        ax.set_zlim(dimension_limits[y_col]["min"], dimension_limits[y_col]["max"])
+        ax.set_ylim(dimension_limits[y_col]["min"], dimension_limits[y_col]["max"])
+    if z_col in dimension_limits:
+        ax.set_zlim(dimension_limits[z_col]["min"], dimension_limits[z_col]["max"])
 
     # Save pictures for each rotation angle
     for angle in range(0, 360, int(step_size)):
         ax.view_init(elev=30, azim=angle)  # Set the elevation and azimuth angles
         parquet_filenames_str = "_".join(shortened_parquet_filenames)
-        filename = f"{plot_dir}scatter_plot_{w_col}_{x_col}_{y_col}_{z_col}_{parquet_filenames_str}_angle_{angle}.png"
+        filename = f"{plot_dir}scatter_plot_{x_col}_{y_col}_{z_col}_{a_col}_{parquet_filenames_str}_angle_{angle}.png"
         plt.savefig(filename)
         print(f"Saved {filename}")
 
@@ -60,10 +60,10 @@ if __name__ == "__main__":
 
     # parse cli
     parser = argparse.ArgumentParser(description="Generate and save rotated scatter plots")
-    parser.add_argument("--w_col", type=str, required=True, help="Name of the column for 'w'")
     parser.add_argument("--x_col", type=str, required=True, help="Name of the column for 'x'")
     parser.add_argument("--y_col", type=str, required=True, help="Name of the column for 'y'")
     parser.add_argument("--z_col", type=str, required=True, help="Name of the column for 'z'")
+    parser.add_argument("--a_col", type=str, required=True, help="Name of the column for 'a'")
     parser.add_argument("--step_size", default=10, type=int, help="Size of rotation step in degrees")
     parser.add_argument("--filenames", nargs="+", required=True, help="List of Parquet filenames")
     args = parser.parse_args()
@@ -79,10 +79,10 @@ if __name__ == "__main__":
         df_list,
         shortened_parquet_filenames,
         plot_dir,
-        args.w_col,
         args.x_col,
         args.y_col,
         args.z_col,
+        args.a_col,
         args.step_size,
         dimension_limits
     )
